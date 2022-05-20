3acffa67cef9ce38e58e9feb78c76733b86ac5d3
Tony Fischetti <tony.fischetti@gmail.com>
2022-05-20 09:09:10 -0400

    more to install script

--- install.sh
+++ install.sh
@@ -8,7 +8,18 @@ ln -s ~/.lisp/.clisprc.lisp ~/.clisprc.lisp
 ln -s ~/.lisp/.eclrc ~/.eclrc
 ln -s ~/.lisp/.clreplrc ~/.clreplrc
 git clone https://github.com/tonyfischetti/pluto ~/pluto
+cd ~/pluto/libstyx
+./compile.sh
+cd $HOME
+mkdir -p ~/quicklisp/local-projects
 ln -s ~/pluto ~/quicklisp/local-projects/pluto
 ln -s ~/.lisp/linedit/ ~/quicklisp/local-projects/linedit
-ln -s ~/.lisp/other-packages/cl-repl/ ~/quicklisp/local-projects/cl-repl
-ln -s ~/.lisp/other-packages/cl-tui/ ~/quicklisp/local-projects/cl-tui
+# ln -s ~/.lisp/other-packages/cl-repl/ ~/quicklisp/local-projects/cl-repl
+# ln -s ~/.lisp/other-packages/cl-tui/ ~/quicklisp/local-projects/cl-tui
+
+sbcl --no-linedit --without-pluto --eval "(quit)"
+sbcl --without-pluto --eval "(quit)"
+sbcl --eval "(quit)"
+
+zsh -c ~/.zsh/bin/update-lisp-cores.sh
+
